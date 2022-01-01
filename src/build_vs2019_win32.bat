88885135a9fdfeb689ce6a3a091d3c061bea1dd6
tomo3136a <tomo3136a@gmail.com>
2022-01-01 16:58:09 +0900

    modified source folder

---
+++ src/build_vs2019_win32.bat
@@ -0,0 +1,17 @@
+rem build vs2019 Win32
+rem
+rem vs2019
+rem cmake 3.19.6
+
+pushd libs
+cmake -DCMAKE_GENERATOR="Visual Studio 16 2019" -DARCHITECTURE=Win32 -P buildall.cmake
+popd
+
+mkdir build_vs2019_win32
+pushd build_vs2019_win32
+cmake.exe .. -G "Visual Studio 16 2019" -A Win32
+cmake.exe --build . --config release
+
+cmake -P cmake_install.cmake
+popd
+pause
---
+++ src/build_vs2019_x64.bat
@@ -0,0 +1,17 @@
+rem build vs2019 x64
+rem
+rem vs2019
+rem cmake 3.19.6
+
+pushd libs
+cmake -DCMAKE_GENERATOR="Visual Studio 16 2019" -DARCHITECTURE=x64 -P buildall.cmake
+popd
+
+mkdir build_vs2019_x64
+pushd build_vs2019_x64
+cmake.exe .. -G "Visual Studio 16 2019"
+cmake.exe --build . --config release
+
+cmake -P cmake_install.cmake
+popd
+pause
